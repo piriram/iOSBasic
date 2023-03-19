63bbd96073989134094a02a6617d1f22f5486197
ram1119 <pyoram25@gmail.com>
2023-03-20 01:29:42 +0900

    5-2-3.코드 완성

--- project/Network in iOS.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
+++ project/Network in iOS.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
@@ -1,7 +1,11 @@
 //: [Previous](@previous)
 
 import Foundation
-
+/*
+ 코더블을 이용해보자
+ App Model < -- JSON : (Decoading)
+ 
+ */
 
 struct GithubProfile: Codable {
     let login: String
@@ -18,6 +22,38 @@ struct GithubProfile: Codable {
         case following
     }
 }
+
+// App Model <-> JSON: (Decoding)
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+
+let url = URL(string: "https://api.github.com/users/priram")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+        print("--> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    // data -> GithubProfile
+    do {
+        let decoder = JSONDecoder()
+        let profile = try decoder.decode(GithubProfile.self, from: data)
+        print("profile: \(profile)")
+    } catch let error as NSError {
+        print("error: \(error)")
+    }
+    
+    let result = String(data: data, encoding: .utf8)
+    print(result)
+}
+
+task.resume()
+//completionHandler:데이터를 요청한 것들
+
+
+
 
 
 
