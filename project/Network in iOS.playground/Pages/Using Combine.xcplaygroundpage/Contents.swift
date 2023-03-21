0abdec542e829a399eb7bfc104ee12dc759abf7e
ram1119 <pyoram25@gmail.com>
2023-03-21 11:39:50 +0900

    5-2-5.완성

--- project/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ project/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -24,12 +24,49 @@ struct GithubProfile: Codable {
     }
 }
 
+final class NetworkService {
+    
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
+        let urlString = "https://api.github.com/users/\(userName)"
+        let url = URL(string: urlString)!
+        
+        return session
+            .dataTaskPublisher(for: url)//서버에서 받은 response 확인 및 data
+            .tryMap { result -> Data in
+                guard let response = result.response as? HTTPURLResponse,
+                      (200..<300).contains(response.statusCode)
+                else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1//stausCode가 없으면
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                //받은 데이타 디코딩 잘하기
+                return result.data
+            }
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
 
 
-
-
+let networkService = NetworkService(configuration: .default)
+let subscription = networkService
+    .fetchProfile(userName: "piriram")
+    .receive(on: RunLoop.main)
+//    .retry(3)
+//retry가 메소드로 있어서 쉽다.
+    .sink { error in
+        print("error: \(error)")
+    } receiveValue: { profile in
+        print("profile: \(profile)")
+    }
 
 //: [Next](@next)
-
 
 
